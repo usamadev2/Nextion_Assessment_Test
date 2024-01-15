06e50ac64af0d32438d8fdc792c6353d6dadd074
usamadev2 <usamadev2@gmail.com>
2024-01-15 07:25:51 -0800

    SharedPreferences service class

--- lib/core/services/db_service.dart
+++ lib/core/services/db_service.dart
@@ -1,14 +1,17 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SharedPreferencesServices {
+  /* ------------------- save data in SharedPreferences class------------------------------- */
   static const String _favoritesKey = 'favorites';
 
+/* ------------------- get data in SharedPreferences ------------------------------- */
   static Future<List<String>> getFavorites() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     final List<String>? favorites = prefs.getStringList(_favoritesKey);
     return favorites ?? [];
   }
 
+  /* ------------------- add data in SharedPreferences ------------------------------- */
   static Future<void> addToFavorites(String movieData) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     List<String> favorites = await getFavorites();
@@ -16,6 +19,7 @@ class SharedPreferencesServices {
     prefs.setStringList(_favoritesKey, favorites);
   }
 
+/* ------------------- remove data in SharedPreferences ------------------------------- */
   static Future<void> removeFromFavorites(String movieTitle) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     List<String> favorites = await getFavorites();
