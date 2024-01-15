65d477cbdc2c27c474cf4f408504b71db2205f00
usamadev2 <usamadev2@gmail.com>
2024-01-15 07:11:26 -0800

    Extension for spacing

--- lib/core/extensions/space_xy.dart
+++ lib/core/extensions/space_xy.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-/// Extension for spacing b/w widgets on num
+/*------------------------ Extension for spacing b/w widgets on num  ------------------------*/
 extension SpaceXY on num {
   Widget get spaceX => SizedBox(width: toDouble());
   Widget get spaceY => SizedBox(height: toDouble());
